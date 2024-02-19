4d7f7d186e3622c70d835495c82d1ff066df8091
dheeraj <dheerajkasaniya140@gmail.com>
2024-02-19 14:21:26 +0530

    inotebook commit

---
+++ iNoteBook/FrontEnd/my-app/src/componets/About.js
@@ -0,0 +1,13 @@
+import React, { useEffect } from 'react'
+import { useContext } from 'react'
+import NotesContext from '../context/notes/NotesContext'
+
+function About() {
+  return (
+    <div>
+      This is about 
+    </div>
+  )
+}
+
+export default About
